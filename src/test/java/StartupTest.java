5d6125c77d2c1d30a7e207fbecfe8033359ada6e
Toby <toby.weston@gmail.com>
2023-10-17 15:19:48 +0000

    basic gradle setup, minimal deps

---
+++ src/test/java/StartupTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StartupTest {
+
+    @Test
+    public void doesMyCodeCompile() {
+        assertTrue(false, "can I run some basic tests?");
+    }
+}
