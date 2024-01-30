4d9554e935b2256b3905f2c69ea26b7fc813645d
ajinkya2006 <43513616+ajinkya2006@users.noreply.github.com>
2024-01-30 11:20:29 +0530

    Update Customer.java

--- src/main/java/org/xpdojo/Customer.java
+++ src/main/java/org/xpdojo/Customer.java
@@ -4,6 +4,13 @@ import java.util.Vector;
 import java.util.Enumeration;
 
 public class Customer {
+	
+	private String name;
+    private Vector rentals = new Vector();
+    
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    
     public Customer(String name) {
         this.name = name;
     }
@@ -17,49 +24,77 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration rentals = this.rentals.elements();
+        
+        Enumeration rentals = getRentals();
         String result = "Rental Record for " + getName() + "\n";
-
+        
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            // determines the amount for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            frequentRenterPoints++;
-
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-
+            
+            thisAmount = getAmountAndUpdateTotalAmount(thisAmount, each);
+            
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";  
+            
+            calculateFrequentRenterPoints(each);
         }
-
-        result += "You owed " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+        
+        result += "You owed " + String.valueOf(getAmountOwed()) + "\n";
+        result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points\n";
 
         return result;
     }
 
+	private double getAmountAndUpdateTotalAmount(double thisAmount, Rental each) {
+		thisAmount = calculateAmount(thisAmount, each);
+		totalAmount+=thisAmount;
+		return thisAmount;
+	}
 
-    private String name;
-    private Vector rentals = new Vector();
+	private void calculateFrequentRenterPoints(Rental each) {
+		frequentRenterPoints++;
+		if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
+		    frequentRenterPoints++;
+	}
+
+	private Enumeration getRentals() {
+		Enumeration rentals = this.rentals.elements();
+		return rentals;
+	}
+    
+    
+    /**
+     * @return totalAmount
+     */
+    public double getAmountOwed() {
+    	return totalAmount;
+    }
+
+	private double calculateAmount(double thisAmount, Rental each) {
+		// determines the amount for each line
+		switch (each.getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        thisAmount += 2;
+		        if (each.getDaysRented() > 2)
+		            thisAmount += (each.getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        thisAmount += each.getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        thisAmount += 1.5;
+		        if (each.getDaysRented() > 3)
+		            thisAmount += (each.getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return thisAmount;
+	}
+    
+    
+    /**
+     * @return The rentals
+     */
+    public int getFrequentRenterPoints() {
+    	return frequentRenterPoints;
+    }
 }
